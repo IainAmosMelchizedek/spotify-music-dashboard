5d40b47ea9c607fd86964279f4c46cea268eb2d1
Iain Melchizedek <iainamos@outlook.com>
2024-12-05 21:04:25 -0500

    Update app.py with correct Gunicorn setup

--- app.py
+++ app.py
@@ -149,6 +149,9 @@ def update_violin_plot(_):
     )
     return fig
 
+# Expose server for Gunicorn
+server = app.server
+
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
